4bd26997ab43fa19a80782ed75df995c93548662
jomarbarnobal <jomar.barnobal@outlook.com>
2016-11-26 02:43:46 -0500

    add controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/create'
+
+  get 'users/edit'
+
+  get 'users/delete'
+
+  get 'users/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
